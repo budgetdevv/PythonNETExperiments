9de3c23ec0976ecb44acb1698785dea67d818404
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-03-08 17:07:43 +0800

    Initial Commit

---
+++ PythonNETExperiments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PythonNETExperiments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
